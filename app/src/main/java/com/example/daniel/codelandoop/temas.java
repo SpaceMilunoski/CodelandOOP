8336f2fb89946682b96a99f7eedb8d34bb44a26c
SpaseMilunoski <dan.soto576@gmail.com>
2018-07-26 23:57:32 -0500

    texto centrado

--- app/src/main/java/com/example/daniel/codelandoop/temas.java
+++ app/src/main/java/com/example/daniel/codelandoop/temas.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
