52e5901c1d155deb408850402295ea808cb18bb7
SpaseMilunoski <dan.soto576@gmail.com>
2018-07-31 01:13:47 -0500

    agregados todos los cuestionarios, faltan preguntas

--- app/src/main/java/com/example/daniel/codelandoop/Cuestionario.java
+++ app/src/main/java/com/example/daniel/codelandoop/Cuestionario.java
@@ -7,10 +7,11 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class Cuestionario extends AppCompatActivity {
-    String url="https://docs.google.com/forms/d/e/1FAIpQLSe9qgbeF2jK8t0UHWYgtLuSvRogGV9FBD7DPLnYBBf3lXBn7w/viewform?usp=sf_link";
+    String url="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        url=getIntent().getStringExtra("url");
         setContentView(R.layout.activity_cuestionario);
         WebView cuestionario = (WebView) findViewById(R.id.wvCuestionario);
         cuestionario.setWebViewClient(new WebViewClient());
